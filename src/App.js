1d470b6a1ec3eeb2492e408718d11fb7ed4d0516
Mihail Yonchev <myonchev99@gmail.com>
2021-05-15 17:09:35 +0300

    Installed json-server to mock requests and db

--- src/App.js
+++ src/App.js
@@ -5,30 +5,7 @@ import { useState } from 'react'
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false)
-  const [tasks, setTasks] = useState([{
-      id: 1,
-      text: 'Doctors Appointment',
-      day: 'Feb 5th at 2:30 pm',
-      reminder: false,
-  },
-  {
-      id: 2,
-      text: 'Beauty Studio',
-      day: 'Feb 5th at 2:30 pm',
-      reminder: false,
-  },
-  {
-      id: 3,
-      text: 'Base Jump Time',
-      day: 'Feb 5th at 2:30 pm',
-      reminder: false,
-  },
-  {
-      id: 4,
-      text: 'Massage Time',
-      day: 'Feb 5th at 2:30 pm',
-      reminder: false,
-  }])
+  const [tasks, setTasks] = useState([])
 
   const name = 'Brad';
   const x = true;
