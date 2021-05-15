2686abc42b12191bfc16a8ed0955bd296877784f
Mihail Yonchev <myonchev99@gmail.com>
2021-05-15 15:45:43 +0300

    Introduced to conditional styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,52 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks';
+import { useState } from 'react'
 
 function App() {
+  const [tasks, setTasks] = useState([{
+      id: 1,
+      text: 'Doctors Appointment',
+      day: 'Feb 5th at 2:30 pm',
+      reminder: false,
+  },
+  {
+      id: 2,
+      text: 'Beauty Studio',
+      day: 'Feb 5th at 2:30 pm',
+      reminder: false,
+  },
+  {
+      id: 3,
+      text: 'Base Jump Time',
+      day: 'Feb 5th at 2:30 pm',
+      reminder: false,
+  },
+  {
+      id: 4,
+      text: 'Massage Time',
+      day: 'Feb 5th at 2:30 pm',
+      reminder: false,
+  }])
+
   const name = 'Brad';
   const x = true;
+
+  // Toggle Reminder
+  const  toggleReminder = (id) => {
+    console.log(id)
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder} : task))
+  }
+
+  // DELETE TASK
+  const deleteTask = (id) => {
+    console.log('delete', id)
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
 
   return (
     <div className="container">
       <Header title = 'tes'/>
-      <Tasks/>
+      <Tasks tasks={tasks} onToggle={toggleReminder} onDelete={deleteTask}/>
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,33 +1,14 @@
-import { useState } from 'react'
+import Task from './Task'
 
-export const Tasks = () => {
-    const [tasks, setTasks] = useState([{
-        id: 1,
-        text: 'Doctors Appointment',
-        day: 'Feb 5th at 2:30 pm',
-        reminder: false,
-    },
-    {
-        id: 2,
-        text: 'Beauty Studio',
-        day: 'Feb 5th at 2:30 pm',
-        reminder: false,
-    },
-    {
-        id: 3,
-        text: 'Base Jump Time',
-        day: 'Feb 5th at 2:30 pm',
-        reminder: false,
-    },
-    {
-        id: 4,
-        text: 'Massage Time',
-        day: 'Feb 5th at 2:30 pm',
-        reminder: false,
-    }])
+export const Tasks = ({tasks, onDelete, onToggle}) => {
     return (
         <>
-            {tasks.map((task) => (<h3 key={task.id}>{task.text}</h3>))}
+            {
+            tasks.length > 0 ?
+            tasks.map((task) => (
+            <Task key={task.id} task={task} onToggle={onToggle} onDelete={onDelete}></Task>
+            )) : 'No Tasks'
+            }
         </>
     )
 }
